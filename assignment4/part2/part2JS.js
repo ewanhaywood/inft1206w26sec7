3c659e58a801a3e4688c818f68d791c79561723a
ewanhaywood <haywoodewan6@gmail.com>
2026-04-07 16:27:38 -0400

    commit 3
    add pictures to folder and add picture objects

--- assignment4/part2/part2JS.js
+++ assignment4/part2/part2JS.js
@@ -8,3 +8,11 @@ const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+
+const images = [
+    {filename: "pic1.jpg", alt: "closeup of a human eye"},
+    {filename: "pic2.jpg", alt: "closeup of a rock"},
+    {filename: "pic3.jpg", alt: "white and purple flowers"},
+    {filename: "pic4.jpg", alt: "wall from a egyptian tomb"},
+    {filename: "pic5.jpg", alt: "a moth of a leaf"},  
+];
