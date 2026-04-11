490ebdb96e13cf5dbcd176e4b4c77cfa8086ec56
ewanhaywood <haywoodewan6@gmail.com>
2026-04-10 21:40:13 -0400

    Update part4JS.js
    add collision detect

--- assignment4/part4/part4JS.js
+++ assignment4/part4/part4JS.js
@@ -163,6 +163,22 @@ class EvilCircle extends Shapes {
       this.y += this.size;
     }
   }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.exists = false;
+          count--;
+          parseFloat.textContent = 'Ball count: ' + count;
+        }
+      }
+    }
+  }
 }
 
 loop();
